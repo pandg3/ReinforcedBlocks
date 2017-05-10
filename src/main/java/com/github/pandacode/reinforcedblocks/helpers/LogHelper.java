439c11aebfa68a20665f0d0c3e687abe9cd4c326
pandg3 <pauloc.panda@gmail.com>
2017-05-09 20:37:38 -0700

    Fixed log errors

--- src/main/java/com/github/pandacode/reinforcedblocks/helpers/LogHelper.java
+++ src/main/java/com/github/pandacode/reinforcedblocks/helpers/LogHelper.java
@@ -4,19 +4,17 @@ package com.github.pandacode.reinforcedblocks.helpers;
 
 import com.github.pandacode.reinforcedblocks.Reference;
 import cpw.mods.fml.common.FMLLog;
-
-import java.util.logging.Level;
-
+import org.apache.logging.log4j.Level;
 
 
-
-/**
- * Created by paulo on 5/9/2017.
- */
 public class LogHelper {
     public static void log(Level logLevel, Object object){FMLLog.log(Reference.NAME, logLevel, String.valueOf(object));}
     public static void all(Object object) {log(Level.ALL, object);}
+    public static void debug(Object object){log(Level.DEBUG, object);}
+    public static void error(Object object){log(Level.ERROR, object);}
+    public static void fatal(Object object){log(Level.FATAL, object);}
     public static void info(Object object){log(Level.INFO, object);}
     public static void off(Object object){log(Level.OFF, object);}
-    public static void warn(Object object){log(Level.WARNING, object);}
+    public static void trace(Object object){log(Level.TRACE, object);}
+    public static void warn(Object object){log(Level.WARN, object);}
 }
